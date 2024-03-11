fbbf8856c9e7ef55c02c0949d6903e918b9843c8
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-03-11 00:30:12 -0300

    Feat: Chapter 2 and 3

---
+++ Chapter 2/function.hs
@@ -0,0 +1,7 @@
+triple x = x * 3
+half x = x /2
+square x = x * x
+perimeter x y = (x * 2) + (y * 2)
+perimeter1 x y = x * 2 + y * 2
+f x = x / 2 + 9
+f1 x = x / (2 + 9)
---
+++ Chapter 3/print2.hs
@@ -0,0 +1,7 @@
+module Main where
+main :: IO ()
+main = do
+    putStrLn "Conte até 4 para mim:"
+    putStr "um , dois"
+    putStr ", três, e"
+    putStrLn " quatro!"
